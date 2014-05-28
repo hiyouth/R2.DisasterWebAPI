1d8d455f7efa0f8c4340219e541f8b80a56a182e
hiyouth <aswindguy@outlook.com>
2014-05-28 15:32:36 +0800

    新增LandFractureMap配置

---
+++ Domain/R2.Disaster.Service/GeoDisaster/Investigation/IComprehensiveService.cs
@@ -0,0 +1,14 @@
+﻿using R2.Disaster.CoreEntities.Domain.GeoDisaster.Investigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace R2.Disaster.Service.GeoDisaster.Investigation
+{
+    public interface IComprehensiveService
+    {
+        Comprehensive GetByUnifiedID(string uid);
+        void New(Comprehensive ghc);
+    }
+}
