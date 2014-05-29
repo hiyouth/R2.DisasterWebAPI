9bec8fcccff516c22c43688e888356ed961d45fd
hiyouth <aswindguy@outlook.com>
2014-05-29 18:05:56 +0800

    新增雨量相关实体

---
+++ Core/R2.Disaster.Core.Entities/Domain/GeoDisaster/Monitor/Rainfall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.CoreEntities.Domain.GeoDisaster.Monitor
+{
+    public class Rainfall
+    {
+        public long Id { get; set; }
+        public String StationId { get; set; }
+        public DateTime CollectTime { get; set; }
+        public double RainfallValue { get; set; }
+    }
+}
---
+++ Core/R2.Disaster.Core.Entities/Domain/GeoDisaster/Monitor/RainfallStation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.CoreEntities.Domain.GeoDisaster.Monitor
+{
+    public class RainfallStation
+    {
+        public String Id { get; set; }
+
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public double Lon { get; set; }
+
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public double Lat { get; set; }
+
+        public string StationName { get; set; }
+
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 所属省名
+        /// </summary>
+        public string Provinc { get; set; }
+
+        /// <summary>
+        /// 所属市名
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// 所属县名
+        /// </summary>
+        public string County { get; set; }
+    }
+}
