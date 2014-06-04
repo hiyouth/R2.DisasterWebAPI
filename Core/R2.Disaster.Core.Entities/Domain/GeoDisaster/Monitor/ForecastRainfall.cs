61389889110df6edef2ea5225fb3e2c8e53f81a2
hiyouth <aswindguy@outlook.com>
2014-06-04 12:02:33 +0800

    完善预报雨量实体属性

--- Core/R2.Disaster.Core.Entities/Domain/GeoDisaster/Monitor/ForecastRainfall.cs
+++ Core/R2.Disaster.Core.Entities/Domain/GeoDisaster/Monitor/ForecastRainfall.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace R2.Disaster.CoreEntities.Domain.GeoDisaster.Monitor
 {
-    class ForecastRainfall
+    public class ForecastRainfall
     {
+        public int Id { get; set; }
+        /// <summary>
+        /// 24H，48H，72H
+        /// </summary>
+        public String DayFlag { get; set; }
+        public DateTime Time { get; set; }
+        public String StationId { get; set; }
+        public double Value { get; set; }
     }
 }
