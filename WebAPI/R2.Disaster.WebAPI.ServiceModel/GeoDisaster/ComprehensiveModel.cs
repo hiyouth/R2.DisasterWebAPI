69db126351ae1da0e9faff727a7a32b13d389c94
hiyouth <aswindguy@outlook.com>
2014-06-04 15:54:09 +0800

    新增服务层实体模型类库

---
+++ WebAPI/R2.Disaster.WebAPI.ServiceModel/GeoDisaster/ComprehensiveModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace R2.Disaster.WebAPI.Model
+{
+    public class ComprehensiveModel
+    {
+    }
+}
