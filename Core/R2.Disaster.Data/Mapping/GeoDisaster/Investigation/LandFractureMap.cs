a2b18872e436b9bc71f0a34c89a2c5b8289ec971
hiyouth <aswindguy@outlook.com>
2014-06-10 12:26:28 +0800

    删除实体部分属性后，需要修改实体的实体关系配置文件

--- Core/R2.Disaster.Data/Mapping/GeoDisaster/Investigation/LandFractureMap.cs
+++ Core/R2.Disaster.Data/Mapping/GeoDisaster/Investigation/LandFractureMap.cs
@@ -10,8 +10,8 @@ namespace R2.Disaster.Data.Mapping.GeoDisaster.Investigation
             this.ToTable("LandFractures");
             this.HasKey(c => c.Id);
 
-            this.Property(c => c.统一编号).IsRequired();
-            this.Property(c => c.名称).IsRequired();
+            //this.Property(c => c.统一编号).IsRequired();
+            //this.Property(c => c.名称).IsRequired();
 
             this.HasRequired(c => c.Comprehensive)
                 .WithRequiredDependent(c => c.LandFracture);
--- Core/R2.Disaster.Data/Mapping/GeoDisaster/Investigation/LandSlipMap.cs
+++ Core/R2.Disaster.Data/Mapping/GeoDisaster/Investigation/LandSlipMap.cs
@@ -15,8 +15,8 @@ namespace R2.Disaster.Data.Mapping.GeoDisaster.Investigation
             this.ToTable("LandSlips");
             this.HasKey(c => c.Id);
 
-            this.Property(c => c.统一编号).IsRequired();
-            this.Property(c => c.名称).IsRequired();
+            //this.Property(c => c.统一编号).IsRequired();
+            //this.Property(c => c.名称).IsRequired();
 
             this.HasRequired(c => c.Comprehensive)
                 .WithRequiredDependent(c => c.LandSlip);
