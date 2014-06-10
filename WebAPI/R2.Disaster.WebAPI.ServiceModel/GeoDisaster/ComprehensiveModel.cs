f0b54302f903701e03e872edab204580c3f7c150
hiyouth <aswindguy@outlook.com>
2014-06-10 14:50:01 +0800

    在ComprehensiveModel中新增部分属性

--- WebAPI/R2.Disaster.WebAPI.ServiceModel/GeoDisaster/ComprehensiveModel.cs
+++ WebAPI/R2.Disaster.WebAPI.ServiceModel/GeoDisaster/ComprehensiveModel.cs
@@ -18,15 +18,21 @@ namespace R2.Disaster.WebAPI.Model
         public string 纬度 { get; set; }
         public int 死亡人数 { get; set; }
         public int 威胁人口 { get; set; }
+
         public double 直接经济损失 { get; set; }
         public double 威胁财产 { get; set; }
         public string 目前稳定状态 { get; set; }
-        public string 灾害规模等级 { get; set; }
+        public string 规模等级 { get; set; }
+
         public string 灾情等级 { get; set; }
         public string 险情等级 { get; set; }
         public double X坐标 { get; set; }
         public double Y坐标 { get; set; }
         public double 灾害体积 { get; set; }
+        public double 灾害面积 { get; set; }
+        public double 标高 { get; set; }
+        public string 今后变化趋势 { get; set; }
+        public string 危害程度 { get; set; }
 
         public string GBCodeId { get; set; }
     }
