580cc2b8f06368a04559b4cd82d2a304317a5db7
hiyouth <aswindguy@outlook.com>
2014-06-16 12:28:49 +0800

    防灾预案实体：新增3个表达式树；新增GetByKeyWords接口

--- Domain/R2.Disaster.Service/GeoDisaster/MassPres/PrePlanService.cs
+++ Domain/R2.Disaster.Service/GeoDisaster/MassPres/PrePlanService.cs
@@ -1,8 +1,10 @@
 ﻿using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
 using R2.Disaster.Repository;
+using R2.Helper.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,7 +29,7 @@ namespace R2.Disaster.Service.GeoDisaster.MassPres
         public PrePlan GetByPhyId(int id)
         {
             var q = from p in this._repoistoryPrePlan.Table
-                    where p.PhyGeoDisaster.Id == id
+                    where p.Id == id
                     select p;
             return q.FirstOrDefault();
         }
@@ -42,10 +44,49 @@ namespace R2.Disaster.Service.GeoDisaster.MassPres
 
         public IQueryable<PrePlan> GetByKeyWord(string keyWord)
         {
-            return null;
+            var eps = DynamicLinqExpressions.False<PrePlan>();
+            eps = eps.Or(this.GetExpressionByUnifiedId(keyWord))
+                .Or(this.GetExpressionByName(keyWord))
+                .Or(this.GetExpressionByLocation(keyWord));
+            IQueryable<PrePlan> preplans = this.ExecuteConditions(eps);
+            return preplans;
         }
 
         #region 表达式树
+        /// <summary>
+        /// 名称查询表达式树
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Expression<Func<PrePlan, Boolean>> GetExpressionByName(string name)
+        {
+            var eps = DynamicLinqExpressions.True<PrePlan>();
+            if (!String.IsNullOrEmpty(name))
+            {
+                eps = eps.And(c => c.名称.Contains(name));
+            }
+            return eps;
+        }
+
+        public Expression<Func<PrePlan, Boolean>> GetExpressionByUnifiedId(string uid)
+        {
+            var eps = DynamicLinqExpressions.True<PrePlan>();
+            if (!String.IsNullOrEmpty(uid))
+            {
+                eps = eps.And(c => c.统一编号.Contains(uid));
+            }
+            return eps;
+        }
+
+        public Expression<Func<PrePlan, Boolean>> GetExpressionByLocation(string keyword)
+        {
+            var eps = DynamicLinqExpressions.True<PrePlan>();
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                eps = eps.And(c => c.地理位置.Contains(keyword));
+            }
+            return eps;
+        }
         #endregion
     }
 }
