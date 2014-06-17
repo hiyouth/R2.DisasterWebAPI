9400b331f08f7d53387dde422917e16110c5bca8
hiyouth <aswindguy@outlook.com>
2014-06-17 17:55:32 +0800

    为MassPreService新增3个接口方法

---
+++ Domain/R2.Disaster.Service/GeoDisaster/MassPres/IMassPreService.cs
@@ -0,0 +1,23 @@
+﻿using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.Service.GeoDisaster.MassPres
+{
+    public interface IMassPreService:IDomainServiceBase<MassPre>
+    {
+        IQueryable<MassPre >GetByUid(string uid);
+
+        /// <summary>
+        /// 关键字将检索名称和统一编号两个属性
+        /// </summary>
+        /// <param name="keyWord"></param>
+        /// <returns></returns>
+        IQueryable<MassPre> GetByKeyWord(string keyWord);
+
+        IQueryable<MassPre> GetByGBCode(string gbcode);
+    }
+}
