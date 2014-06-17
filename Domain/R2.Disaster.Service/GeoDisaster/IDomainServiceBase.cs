3d9c6676348f85010177823835523e3531ab373f
hiyouth <aswindguy@outlook.com>
2014-06-17 13:56:21 +0800

    新增IDomainServiceBase基础接口

---
+++ Domain/R2.Disaster.Service/GeoDisaster/IDomainServiceBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.Service.GeoDisaster
+{
+    public interface IDomainServiceBase<T> where T:class
+    {
+        void Update(T preplan);
+
+         void Delete(T preplan);
+
+         void Delete(int id);
+
+        void New(T preplan);
+
+         T Get(int id);
+    }
+}
