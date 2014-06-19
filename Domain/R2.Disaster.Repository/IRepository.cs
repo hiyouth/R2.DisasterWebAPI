ca5f26c84d2bb60c6bfeac156ba88ed1d319fd52
hiyouth <aswindguy@outlook.com>
2014-06-20 02:54:39 +0800

    新增操作一组实体的Repository接口

--- Domain/R2.Disaster.Repository/IRepository.cs
+++ Domain/R2.Disaster.Repository/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace R2.Disaster.Repository
 {
@@ -9,9 +10,19 @@ namespace R2.Disaster.Repository
     {
         T GetById(object id);
         void Insert(T entity);
+
+        void Insert(IList<T> entities);
         void Update(T entity);
+
+        void Update(IList<T> entities);
         void Delete(T entity);
+
+        void Delete(IList<T> entities);
+
         void Delete(object id);
+
+        void Delete(IList<object> id);
+
         IQueryable<T> Table { get; }
     }
 }
