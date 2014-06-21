2a76f95f7694adc1b2b52c4d9d3335d0c854892e
hiyouth <aswindguy@outlook.com>
2014-06-21 10:22:40 +0800

    新增IEnityServiceBase接口

---
+++ Domain/R2.Disaster.Service/IEntityServiceBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.Service.GeoDisaster
+{
+    public interface IEntityServiceBase<T> where T:class
+    {
+
+        void Update(T entity);
+
+        void Update(IList<T> entities);
+
+        void Delete(T entity);
+
+        void Delete(IList<T> entities);
+
+         void Delete(object id);
+
+         void Delete(IList<object> ids);
+
+         void New(T entity);
+
+         void New(IList<T> entities);
+
+         T Get(object id);
+    }
+}
