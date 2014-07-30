c09edb5fe9672752a23957c70845c09f60c14bdd
hiyouth <aswindguy@outlook.com>
2014-07-30 14:44:53 +0800

    新增了对“隐患点”Threat的 关系配置

--- Core/R2.Disaster.Data/Mapping/GeoDisaster/PhygeoDisasterMap.cs
+++ Core/R2.Disaster.Data/Mapping/GeoDisaster/PhygeoDisasterMap.cs
@@ -32,6 +32,9 @@ namespace R2.Disaster.Data.Mapping.GeoDisaster
             //配置同PrePlan实体的关系，1对1，PrePlan的主键也是其外键
             this.HasRequired(p => p.PrePlan).WithRequiredPrincipal();
 
+            //配置同PrePlan实体的关系，1对1，PrePlan的主键也是其外键
+            this.HasRequired(p => p.Threat).WithRequiredPrincipal();
+
             //配置同MassPre实体的关系，1对1，MassPre的主键也是其外键
             this.HasRequired(p => p.MassPre).WithRequiredPrincipal();
 
