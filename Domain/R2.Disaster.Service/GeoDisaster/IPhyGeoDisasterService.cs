71fab087cf0589e6adf5846f91f6b80b0e4612a4
hiyouth <aswindguy@outlook.com>
2014-08-08 16:49:37 +0800

    新增ExecuteConditions接口

--- Domain/R2.Disaster.Service/GeoDisaster/IPhyGeoDisasterService.cs
+++ Domain/R2.Disaster.Service/GeoDisaster/IPhyGeoDisasterService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,14 @@ namespace R2.Disaster.Service.GeoDisaster
         void New(PhyGeoDisaster phy);
         void Update(PhyGeoDisaster phy);
         PhyGeoDisaster GetById(int id);
+
+        /// <summary>
+        /// 根据条件，返回符合条件的集合
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IQueryable<PhyGeoDisaster> ExecuteConditions(Expression<Func<PhyGeoDisaster , bool>> condition);
+
         IQueryable<PhyGeoDisaster> GetByConditions(List<string> gbcodeIds, List<EnumGeoDisasterType?> types);
         IQueryable<PhyGeoDisaster> GetByConditions(string gbCode, EnumGeoDisasterType? type);
         IQueryable<PhyGeoDisaster> GetByKeyWord(string keyword);
