6579eec4b30fe3c868b4149fd8ef9a699154a5d8
nikolasx <lixiong0901@163.com>
2014-08-19 17:22:34 +0800

    隐患点Controller，防治规划服务。lx

---
+++ Domain/R2.Disaster.Service/GeoDisaster/MassPres/PreventionPlanningService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
+using R2.Disaster.Repository;
+
+namespace R2.Disaster.Service.GeoDisaster.MassPres
+{
+    public class PreventionPlanningService:EntityServiceBase<PreventionPlanning>
+        ,IPreventionPlanningService
+    {
+
+        private IRepository<PreventionPlanning> _repositoryPlanning;
+
+        public PreventionPlanningService(IRepository<PreventionPlanning> repositoryPlanning)
+            :base(repositoryPlanning)
+        {
+            this._repositoryPlanning = repositoryPlanning;
+        }
+    }
+}
---
+++ WebAPI/R2.Disaster.WebAPI/Controllers/GeoDisaster/MassPres/PreventionPlanningController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
+using R2.Disaster.Service.GeoDisaster.MassPres;
+
+namespace R2.Disaster.WebAPI.Controllers.GeoDisaster.MassPres
+{
+    /// <summary>
+    /// 防治规划
+    /// </summary>
+    public class PreventionPlanningController:EntityControllerBase<PreventionPlanning>
+    {
+        private IPreventionPlanningService _planService;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="planService"></param>
+        public PreventionPlanningController(IPreventionPlanningService planService)
+            : base(planService)
+        {
+            this._planService = planService;
+        }
+
+    }
+}
