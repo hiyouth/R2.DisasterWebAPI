49f3c44da409520a8b4d61c4ebee3e4a770cab36
nikolasx <lixiong0901@163.com>
2015-01-06 00:41:10 +0800

    工作明白卡服务

---
+++ Domain/R2.Disaster.Service/GeoDisaster/MassPres/IWorkingGuideCardService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
+
+namespace R2.Disaster.Service.GeoDisaster.MassPres
+{
+    public interface IWorkingGuideCardService : IPhyRelationEntityService<WorkingGuideCard>, ICanExecuteExpress<WorkingGuideCard>
+    {
+
+        /// <summary>
+        /// 通过统一编号查询工作明白卡
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <returns></returns>
+        IQueryable<WorkingGuideCard> GetByUid(string uid);
+    }
+}
