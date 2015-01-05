0527153ed89db3639bec39340d27d319e8249704
nikolasx <lixiong0901@163.com>
2015-01-06 00:18:31 +0800

    避灾明白卡服务

---
+++ Domain/R2.Disaster.Service/GeoDisaster/MassPres/AvoidRiskCardService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
+using R2.Disaster.Repository;
+
+namespace R2.Disaster.Service.GeoDisaster.MassPres
+{
+    public class AvoidRiskCardService : PhyRelationEntityService<AvoidRiskCard>, IAvoidRiskCardService
+    {
+
+        private IRepository<AvoidRiskCard> _repository;
+        public AvoidRiskCardService(IRepository<AvoidRiskCard> repository)
+            : base(repository)
+        {
+            this._repository = repository;
+        }
+
+        public IQueryable<AvoidRiskCard> GetByUId(string uid)
+        {
+            var query = from c in this._repository.Table
+                        where c.统一编号 == uid
+                        select c;
+            return query;
+        }
+    }
+}
