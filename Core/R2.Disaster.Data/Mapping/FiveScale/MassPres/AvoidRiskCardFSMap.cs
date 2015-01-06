d3dbac0108f014eecd6bf467745ed208ba096587
hiyouth <aswindguy@outlook.com>
2015-01-06 15:52:44 +0800

    333

---
+++ Core/R2.Disaster.Data/Mapping/FiveScale/MassPres/AvoidRiskCardFSMap.cs
@@ -0,0 +1,28 @@
+﻿using Corner.Core;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.Investigation;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.MassPres;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.Data.Mapping.GeoDisaster.MassPres
+{
+    public class AvoidRiskCardFSMap : EntityTypeConfiguration<AvoidRiskCardFS>
+    {
+        public AvoidRiskCardFSMap()
+        {
+            this.ToTable("AvoidRiskCardFSes");
+            this.HasKey(c => c.Id);
+
+            //this.Property(c => c.统一编号).IsRequired();
+            this.Property(c => c.名称).IsRequired();
+
+            //this.HasRequired(a => a.PhyGeoDisaster)
+            //   .WithMany(p => p.AvoidRiskCards)
+            //   .HasForeignKey(a => a.PhyGeoDisasterId);
+        }
+    }
+}
