0304d99a389d5caf565af99514305966183b19bb
hiyouth <aswindguy@outlook.com>
2015-01-06 17:23:03 +0800

    6666

---
+++ Core/R2.Disaster.Data/Mapping/FiveScale/Investigation/LandFractureFSMap.cs
@@ -0,0 +1,21 @@
+﻿using Corner.Core;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.Investigation;
+using System.Data.Entity.ModelConfiguration;
+
+namespace R2.Disaster.Data.Mapping.GeoDisaster.Investigation
+{
+    public class LandFractureFSMap : EntityTypeConfiguration<LandFractureFS>
+    {
+        public LandFractureFSMap()
+        {
+            this.ToTable("LandFractureFSes");
+            this.HasKey(c => c.Id);
+
+            //this.Property(c => c.统一编号).IsRequired();
+            //this.Property(c => c.名称).IsRequired();
+
+            //this.HasRequired(c => c.Comprehensive)
+            //    .WithRequiredDependent(c => c.LandFracture);
+        }
+    }
+}
---
+++ Core/R2.Disaster.Data/Mapping/FiveScale/Investigation/LandSlipFSMap.cs
@@ -0,0 +1,26 @@
+﻿using Corner.Core;
+using R2.Disaster.CoreEntities.Domain.GeoDisaster.Investigation;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2.Disaster.Data.Mapping.GeoDisaster.Investigation
+{
+    public class LandSlipFSMap : EntityTypeConfiguration<LandSlipFS>
+    {
+        public LandSlipFSMap()
+        {
+            this.ToTable("LandSlipFSes");
+            this.HasKey(c => c.Id);
+
+            //this.Property(c => c.统一编号).IsRequired();
+            //this.Property(c => c.名称).IsRequired();
+
+            //this.HasRequired(c => c.Comprehensive)
+            //    .WithRequiredDependent(c => c.LandSlip);
+        }
+    }
+}
